09bf793aa2f64e657f84ffadab328ade93216901
leolee <chunhoi22@yahoo.com.hk>
2020-07-27 17:11:53 +0800

    version 2

--- practice1.py
+++ practice1.py
@@ -13,5 +13,24 @@
 #             break
 
 import random
-r=random.randint(1,100)
+start=input("start number:")
+end=input("end number")
+start = int(start)
+end = int(end)
+r=random.randint(start,end)
+count = 0
+
+
 while True:
+    count = count + 1
+    num = input("enter your number:")
+    num = int(num)
+    if num == r:
+        print("You've got it")
+        print(count,"times")
+        break
+    elif num > r :
+        print("it is bigger than random number")
+    elif num < r :
+        print("it is smaller than random number")
+    print(count,"times")
