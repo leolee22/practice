c1e5384cbaf32d01e98e757c459eb550993561a6
leolee22 <chunhoi22@yahoo.com.hk>
2020-07-27 23:43:12 +0800

    v

--- practice1.py
+++ practice1.py
@@ -1,23 +1,9 @@
-﻿# password = "a123456"
-# i=3
-# while True:
-#     pw=input("entry your password:")
-#     if pw == password:
-#         print("success")
-#         break
-#     else:
-#         i = i-1
-#         print("password error, you have ",i,"chance")
-#         if  i==0:
-#             print("lock your account!")
-#             break
-
-import random
-start=input("start number:")
-end=input("end number")
+﻿import random
+start = input("start number:")
+end = input("end number")
 start = int(start)
 end = int(end)
-r=random.randint(start,end)
+r = random.randint(start, end)
 count = 0
 
 
@@ -27,10 +13,10 @@ while True:
     num = int(num)
     if num == r:
         print("You've got it")
-        print(count,"times")
+        print(count, "times")
         break
-    elif num > r :
+    elif num > r:
         print("it is bigger than random number")
-    elif num < r :
+    elif num < r:
         print("it is smaller than random number")
-    print(count,"times")
+    print(count, "times")
