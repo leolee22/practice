e1c2a4e82e3ffcb89879a201ec8bdc8094f83c84
leolee <chunhoi22@yahoo.com.hk>
2020-07-27 16:30:08 +0800

    first version

---
+++ practice1.py
@@ -0,0 +1,17 @@
+﻿# password = "a123456"
+# i=3
+# while True:
+#     pw=input("entry your password:")
+#     if pw == password:
+#         print("success")
+#         break
+#     else:
+#         i = i-1
+#         print("password error, you have ",i,"chance")
+#         if  i==0:
+#             print("lock your account!")
+#             break
+
+import random
+r=random.randint(1,100)
+while True:
