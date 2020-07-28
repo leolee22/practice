2a83f804e7e6df49e182e69a9841df40e5d44fd6
leolee <chunhoi22@yahoo.com.hk>
2020-07-28 17:48:42 +0800

    v

--- products.py
+++ products.py
@@ -1,5 +1,12 @@
 products = []
 #---------------------------------------------------
+with open("products.csv","r",encoding="utf-8") as f:
+    for line in f:
+        s = line.strip().split(",") 
+        if "商品名稱,商品價格" in line: # 如果"商品名稱,商品價格" 存在 line
+            continue # 跳過
+        print(s)
+#---------------------------------------------------
 while True:
     name = input("請輸入商品名稱:")
     if name =="q":
@@ -14,6 +21,7 @@ for p in products:
     print(p[0],"價格是",p[1])
 
 #---------------------------------------------------
-with open("products.csv","w") as f:
+with open("products.csv","w",encoding="utf-8") as f:
+    f.write("商品名稱,商品價格\n")
     for g in products:
         f.write(g[0] + "," + str(g[1]) + "\n")    
