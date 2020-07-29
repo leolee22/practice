e3d5e9fd4d4f93cb112b6d0f690eba0cc39a7678
leolee <chunhoi22@yahoo.com.hk>
2020-07-29 09:49:09 +0800

    v

--- products.py
+++ products.py
@@ -1,12 +1,23 @@
+import os
+
 products = []
-#---------------------------------------------------
-with open("products.csv","r",encoding="utf-8") as f:
-    for line in f:
-        s = line.strip().split(",") 
-        if "商品名稱,商品價格" in line: # 如果"商品名稱,商品價格" 存在 line
-            continue # 跳過
-        print(s)
-#---------------------------------------------------
+products2 = []
+
+#檢查檔案是否存在
+if os.path.isfile("products.csv"):
+    print("找到了檔案")
+#讀取已寫入的檔案
+    with open("products.csv","r",encoding="utf-8") as f:
+        for line in f:
+            name,price = line.strip().split(",") 
+            if "商品名稱,商品價格" in line: # 如果"商品名稱,商品價格" 存在 line
+                continue # 跳過
+            products2.append([name,price])
+    print(products2)
+else:
+    print("找不到了檔案")
+        
+#讓使用者輸入
 while True:
     name = input("請輸入商品名稱:")
     if name =="q":
@@ -16,11 +27,11 @@ while True:
     products.append([name,price])
 print(products)
 
-#---------------------------------------------------
+#印出所有購買記錄
 for p in products:
     print(p[0],"價格是",p[1])
 
-#---------------------------------------------------
+#寫入檔案
 with open("products.csv","w",encoding="utf-8") as f:
     f.write("商品名稱,商品價格\n")
     for g in products:
